5b98ff5cbf8d07ad76e8afc4f06e5bbbebc817cc
Jonathan Legrand <jonathan.a.legrand@inria.fr>
2025-03-27 11:02:31 +0100

    Generate corresponding metadata

--- generate_mock_data.py
+++ generate_mock_data.py
@@ -21,6 +21,7 @@ metadata = pd.read_excel(datapath / "metadata.xlsx")
 
 fcs_path = datapath / "0001.fcs"
 xml_path = datapath / "000003.xml"
+random.seed(1234)
 
 def capitalize_lastname(string):
     split = string.split(" ")
@@ -44,19 +45,60 @@ def random_capitalizing(string):
     )
     return func(string)
 
+def write_analysis(fname, output_path="mock_dataset"):
+    """
+    Impure function! TODO There's nothing in the analysis files!!!!
+    """
+    path = f"/tmp/{fname}"
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+    
+    shutil.make_archive(f"{output_path}/{fname}", "zip", path)
+    os.rename(f"{output_path}/{fname}.zip", f"{output_path}/{fname}.analysis")
+    shutil.rmtree(path)
+    
+
+def generate_id(length):
+    digits = (str(random.randint(0, 9)) for _ in range(length))
+    return "".join(digits)
+
+def generate_patient_dict(name):
+    patient_dict = dict()
+    randname_metadata = random_capitalizing(name)
+    split_name = randname_metadata.split(" ")
+    last_names, first_name = split_name[:-1], split_name[-1]
+
+    for element in last_names:
+        if '(' in element:
+            joining_char = " "
+        else:
+            joining_char = ""
+
+    patient_dict["Identifiant patient (NIP)"] = int(generate_id(9))
+    patient_dict["Numero clinisight"] = int(generate_id(8))
+    patient_dict["NOM"] = joining_char.join(last_names)
+    patient_dict["Prénom"] = first_name
+    patient_dict["sexe             (1=H, 2=F)"] = random.choice((1, 2))
+    return patient_dict
+
+
 if __name__ == "__main__":
     with open("fake_names.txt", "r") as f:
         names = f.read().splitlines()
+    fake_metadata = []
     for name in names:
         source = random_capitalizing(
             random.choice(("Moelle", "Sang"))
         )
-        randname = random_capitalizing(name)
-        fname = f"{randname} {source} blabla blabla-bla_bla"
-        path = f"/tmp/{fname}"
-        if os.path.exists(path):
-            shutil.rmtree(path)
-        os.mkdir(path)
-    
-        shutil.make_archive(f"mock_dataset/{fname}", "zip", path)
-        os.rename(f"mock_dataset/{fname}.zip", f"mock_dataset/{fname}.analysis")
+        randname_analysis = random_capitalizing(name)
+        fname = f"{randname_analysis} {source} blabla blabla-bla_bla"
+
+        patient_dict = generate_patient_dict(name)
+        fake_metadata.append(patient_dict)
+        
+    fake_metadata = pd.DataFrame(fake_metadata)
+    # TODO Add random columns
+    #for i in range(10):
+    #    dtype = random.choice((int, float, str))
+    #    data = random.ra
