2fa50ec0e4c9546e26cf0ac97d9fa6767356d2c9
Jonathan Legrand <jonathan.a.legrand@inria.fr>
2025-03-27 16:50:53 +0100

    Match on bdx-id naming convention

--- fcs_anonymisation/matching.py
+++ fcs_anonymisation/matching.py
@@ -4,20 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def is_junk(character):
-    if character in ("(", ")", "_"):
+    if character in ("(", ")", "_", " ", "BDX-"):
         return True
     return False
 
 def best_matching_row(file_id, metadata):
     """
+    Search for similarity with ID or name (depends if it starts with BDX) 
     This could probably be better optimized, because for now we
     are using the whole fname. We know that the interesting
     stuff for matching happens at the beginning
     """
     file_id = file_id.lower()
     concatnames = metadata["NOM"] + " " + metadata["Prénom"]
-    candidates = concatnames.apply(lambda string: string.lower()).to_list()
+    patient_ids = metadata["ID"].apply(lambda string: string.lower())
+    names = concatnames.apply(lambda string: string.lower()).to_list()
 
+    if file_id[:3] == "bdx":
+        candidates = patient_ids
+    else:
+        candidates = names
+        
     similarities = []
     for candidate in candidates:
         sim = SM(is_junk, file_id, candidate)
@@ -25,10 +32,8 @@ def best_matching_row(file_id, metadata):
 
     best_match_idx = np.argmax(similarities)
     max_sim = similarities[best_match_idx]
-    if max_sim < 0.3: # This threshold is somewhat arbitrary
-        raise ValueError(f"Max similarity = {max_sim} < 0.3")
 
     best_match = candidates[best_match_idx]
     print(file_id)
     print(best_match, " is best match", end="\n\n")
-    return best_match, similarities[best_match_idx]
+    return metadata.iloc[best_match_idx, :], max_sim
