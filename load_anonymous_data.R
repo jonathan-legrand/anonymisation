3d5a6ed6ea636ed3dcd8f1f333710348398de489
Jonathan Legrand <jonathan.a.legrand@inria.fr>
2025-08-21 17:50:02 +0200

    Compare compensations

--- load_anonymous_data.R
+++ load_anonymous_data.R
@@ -1,9 +1,11 @@
 library(flowCore)
 library(ggplot2)
 library(glue)
+library(purrr)
 library(ggcyto, help, pos = 2, lib.loc = NULL)
 library(gridExtra)
 library(plot.matrix)
+library(patchwork)
 
 fname <- "mock_output/sub-183205182_specimen-blood/sub-183205182_specimen-blood_sample.fcs"
 csv_name <- "mock_output/sub-183205182_specimen-blood/sub-183205182_specimen-blood_compensation.csv"
@@ -24,18 +26,20 @@ autoplot(x, "FS.A", "SS.A", bins = 200, transorm = FALSE) +
   scale_x_continuous(limits = c(0, 1e7)) +
   theme_minimal()
 
-plot(manual_compensation)
 comp <- spillover(x)$'$SPILLOVER'
-plot(comp)
 fluo_names <- paste(paste("FL", 1:10, sep = ""), "A", sep = ".")
 colnames(comp) <- fluo_names
 colnames(manual_compensation) <- fluo_names
 
-x_comp <- compensate(x, manual_compensation)
+require(gridExtra)
+mat1 <- plot(manual_compensation)
+mat2 <- plot(comp)
+
+x_comp_manual <- compensate(x, manual_compensation)
+x_comp_original <- compensate(x, comp)
 
 channels_of_interest <- c("FL5.A", "FL4.A")
 trans_list <- estimateLogicle(x, channels_of_interest)
-comp_trans_list <- estimateLogicle(x_comp, channels_of_interest)
 #trans <- logicleTransform(
 #  transformationId = "Manual transformation",
 #  w = 0, a = 0, t = 1048575, m = 4.5
@@ -43,6 +47,11 @@ comp_trans_list <- estimateLogicle(x_comp, channels_of_interest)
 #trans_list <- transformList(channels_of_interest, trans)
 
 N_BINS <- 200
+width <- 4
+height <- 4
+xrange <- c(-2, 4)
+yrange <- c(-2, 4)
+
 p1 <- autoplot(
   transform(x, trans_list),
   channels_of_interest[[1]],
@@ -51,30 +60,59 @@ p1 <- autoplot(
   transorm = FALSE
 ) +
   ggtitle("Uncompensated") +
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5)) +
+  coord_cartesian(xlim = xrange, ylim = yrange)
 
 ggsave(
   "plots/raw.png",
   plot = p1,
-  width = 10,
-  height = 8,
+  width = width,
+  height = height,
   units = "cm"
 )
 
 p2 <- autoplot(
-  transform(x_comp, comp_trans_list),
+  transform(x_comp_original, trans_list),
   channels_of_interest[[1]],
   channels_of_interest[[2]],
   bins = N_BINS,
   transform = FALSE
 ) +
-  ggtitle(glue("Compensated")) +
-  theme(plot.title = element_text(hjust = 0.5))
+  ggtitle(glue("Compensated (Default matrix)")) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  coord_cartesian(xlim = xrange, ylim = yrange)
 
 ggsave(
   "plots/compensated.png",
   plot = p2,
-  width = 10,
-  height = 8,
+  width = width,
+  height = height,
   units = "cm"
+)
+
+p3 <- autoplot(
+  transform(x_comp_manual, trans_list),
+  channels_of_interest[[1]],
+  channels_of_interest[[2]],
+  bins = N_BINS,
+  transform = FALSE
+) +
+  ggtitle(glue("Compensated (XML matrix)")) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  coord_cartesian(xlim = xrange, ylim = yrange)
+
+ggsave(
+  "plots/compensated_manual.png",
+  plot = p2,
+  width = width,
+  height = height,
+  units = "cm"
+)
+
+plots <- list(p1, p2, p3)
+plots <- map(plots, as.ggplot)
+wrap_plots(
+  plots,
+  ncol = 3,
+  heights = c(2, 2, 2)
 )
