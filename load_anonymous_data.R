39575ee09fc8792be2a9d0d24fa86ac5f50c79cc
Jonathan Legrand <jonathan.a.legrand@inria.fr>
2025-08-21 16:25:46 +0200

    paths

--- load_anonymous_data.R
+++ load_anonymous_data.R
@@ -5,8 +5,8 @@ library(ggcyto, help, pos = 2, lib.loc = NULL)
 library(gridExtra)
 library(plot.matrix)
 
-fname <- "mock_output/sub-108731871_specimen-unknown/sub-108731871_specimen-unknown_sample.fcs"
-csv_name <- "mock_output/sub-108731871_specimen-unknown/sub-108731871_specimen-unknown_compensation.csv"
+fname <- "mock_output/sub-183205182_specimen-blood/sub-183205182_specimen-blood_sample.fcs"
+csv_name <- "mock_output/sub-183205182_specimen-blood/sub-183205182_specimen-blood_compensation.csv"
 x <- read.FCS(
   fname,
   alter.name = TRUE,
@@ -18,7 +18,6 @@ manual_compensation <- t(as.matrix(read.csv(
   header = TRUE,
   row.names = 1
 )))
-spill_str <- "10,FL1,FL2,FL3,FL4,FL5,FL6,FL7,FL8,FL9,FL10,1,0.0089999999999999976,0.0030000000000000005,0,0,0,0,0,0,0,0.23,1,0.098,0,0.009,0,0,0,0,0,0.074,0.395,1,0,0.004,0,0,0,0,0,0.008,0.07,0.272,1,0.001,0.001,0,0,0,0,0.004,0.019,0.099,0.562,1,0.001,0.003,0.022000000000000002,0,0,0,0,0.003,0.09,0,1,0.047,0.113,0,0,0,0,0,0.77100000000000013,0.008,0.581,1,0.14300000000000002,0,0,0,0,0.001,0.17600000000000002,0.088,0.089,0.176,1,0,0,0,0,0,0,0,0,0,0,1,0.023,0.078,0.181,0.012,0,0.002,0,0,0,0.141,1"
 
 summary(x)
 autoplot(x, "FS.A", "SS.A", bins = 200, transorm = FALSE) +
