12face96bb066d8d80bc8996996958f4c3d24f19
Jonathan Legrand <jonathan.a.legrand@inria.fr>
2025-08-21 16:25:19 +0200

    Fix $ sign bug in regex

--- R_functions/sample_anonymisation.R
+++ R_functions/sample_anonymisation.R
@@ -2,11 +2,6 @@ library(flowCore)
 library(ggcyto)
 library(gridExtra)
 
-path <- file.path(
-  "mock_dataset",
-  "Lefèvre_Rousseau Léa Moelle bla bla-bla_bla 8903.analysis"
-)
-
 essential_fcs_keywords <- scan(
   "metadata_white_list.txt",
   what = "character",
@@ -20,11 +15,22 @@ anonymize_sample <- function(fpath, white_list = essential_fcs_keywords) {
   xml_temp_fpath <- temp_fpaths[grep(".*xml$", temp_fpaths)]
   fcs_temp_fpath <- temp_fpaths[grep(".*fcs$", temp_fpaths)]
   sample <- read.FCS(
-    fcs_temp_fpath, dataset = 2
+    fcs_temp_fpath,
+    dataset = 2,
+    transformation = FALSE,
+    truncate_max_range = FALSE
   )
   mat <- exprs(sample)
   params <- parameters(sample)
-  anonymous_keywords <- keyword(sample)[white_list]
+  kws <- keyword(sample)
+  pattern <- paste0("\\Q", white_list, "\\E", collapse = "|")  # quote metachars
+  matches <- grep(
+    pattern,
+    names(kws),
+    value = TRUE
+  )
+  anonymous_keywords <- kws[matches]
+
   anonymous_fcs <- new("flowFrame",
     exprs = mat, description = anonymous_keywords,
     parameters = params
@@ -32,7 +38,3 @@ anonymize_sample <- function(fpath, white_list = essential_fcs_keywords) {
   return_values <- c(anonymous_fcs, xml_temp_fpath)
   return(return_values)
 }
-
-
-#res <- anonymize_sample(path, essential_fcs_keywords)
-#spill_str <- '10,FL1,FL2,FL3,FL4,FL5,FL6,FL7,FL8,FL9,FL10,1,0.0090000000000000011,0.0030000000000000005,0,0.002,0,0,0,0,0,0.23,1,0.098,0,0.009,0,0,0,0,0,0.074,0.405,1,0,0.10400000000000002,0,0,0,0,0,0.008,0.07,0.272,1,0.038999999999999993,0.001,0,0,0,0,0.004,0.019,0.099,0.562,1,0.001,0.003,0.012,0,0,0,0,0.003,0.09,0,1,0.047,0.053,0,0,0,0,0.003,0.811,0.008,0.601,1,0.113,0,0,0,0,0.001,0.186,0.088,0.099,0.176,1,0,0,0,0,0,0,0.0060000000000000019,0,0,0,1,0.023,0.078,0.181,0.012,0,0.002,0,0,0,0.141,1'
